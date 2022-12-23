fefb34745122ca2c2206387b460e5f8c3e2eb5ed
Jonathan <jonathanmoorevla@gmail.com>
2022-12-22 19:18:47 -0600

    Meet all deliverables

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,10 @@
 class Audition < ActiveRecord::Base
   belongs_to :role
+
+    def call_back
+        self.update(hired: TRUE)
+
+    end
+
+
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,34 @@
+require 'pry'
+
 class Role < ActiveRecord::Base
 
     has_many :auditions
     
+    def actors
+        actors_array = []
+        for audition in self.auditions do 
+            actors_array << audition.actor
+        end
+        return actors_array    
+    end
+
+    def locations
+        locations_array = []
+        for audition in self.auditions do
+            locations_array << audition.location 
+        end
+        return locations_array
+    end
+
+    def lead
+        self.auditions.find_by(hired: TRUE)
+    end
+
+    def understudy
+        hireds = self.auditions.select{|audition| audition.hired}
+        hireds.second
+    end
+
+
 
 end
